fcf3d256f7963c8c7a9466cdcd5d85804d18ce23
Shruti Subhashrao Anerao <135962260+SHRU1703@users.noreply.github.com>
2023-12-13 11:53:02 +0530

    Add files via upload

---
+++ A15_springwebsocket/src/test/java/com/springwebsocket/A15SpringwebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A15SpringwebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
